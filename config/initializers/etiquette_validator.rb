3d534869844d64b4ae5fbbc8fd5096a7e44ef7ca
Vera Rojman <vrojman@protonmail.com>
2020-11-30 12:10:30 +0100

    Hack form for proposal creation wizard

--- config/initializers/etiquette_validator.rb
+++ config/initializers/etiquette_validator.rb
@@ -1,19 +1,22 @@
 # frozen_string_literal: true
 
 # Monkey patch EtiquetteValidator to:
-# - Allow up to 50% uppercase characters
-# - Min word-length 7 characters
+# - Allow up to 100% uppercase characters
+# - Min word-length 3 characters
 Rails.application.config.to_prepare do
   EtiquetteValidator.class_eval do
     private
 
     # accept 100% uppercases
-    def validate_caps(_record, _attribute, _value)
-      nil
+    def validate_caps(record, attribute, value)
+      return if value.scan(/[A-Z]/).length < value.length * options.fetch(:uppercase, 0.25)
+
+      record.errors.add(attribute, options[:message] || :too_much_caps)
     end
 
+    # accept words with 3 characters or more
     def validate_length(record, attribute, value)
-      return if value.length >= 3
+      return if value.length >= options.fetch(:minimum, 3)
 
       record.errors.add(attribute, options[:message] || :too_short)
     end
