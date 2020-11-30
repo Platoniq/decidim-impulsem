320a8a99d91e382aff5b2dce2b80e655ca9b22ce
Vera Rojman <vrojman@protonmail.com>
2020-11-30 11:33:00 +0100

    Fix rubocop offenses

--- config/initializers/etiquette_validator.rb
+++ config/initializers/etiquette_validator.rb
@@ -5,16 +5,16 @@
 # - Min word-length 7 characters
 Rails.application.config.to_prepare do
   EtiquetteValidator.class_eval do
-
     private
 
     # accept 100% uppercases
-    def validate_caps(record, attribute, value)
-      return
+    def validate_caps(_record, _attribute, _value)
+      nil
     end
 
     def validate_length(record, attribute, value)
       return if value.length >= 3
+
       record.errors.add(attribute, options[:message] || :too_short)
     end
   end
